c24f3979581b60d0fe6a22a93552240da243e35d
EricGui <936742342@qq.com>
2025-03-11 22:13:43 +0800

    design main menu

--- conceptListDemo/src/components/conceptBox.vue
+++ conceptListDemo/src/components/conceptBox.vue
@@ -1,12 +1,80 @@
 <template>
-  <a-space wrap>
-    <a-button type="primary">Primary Button</a-button>
-    <a-button>Default Button</a-button>
-    <a-button type="dashed">Dashed Button</a-button>
-    <a-button type="text">Text Button</a-button>
-    <a-button type="link">Link Button</a-button>
-  </a-space>
+  <a-layout class="layout">
+    <a-layout-header>
+      <div class="logo" />
+      <a-menu
+        v-model:selectedKeys="selectedKeys"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">概念列表</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+      <a-breadcrumb style="margin: 16px 0">
+        <a-breadcrumb-item>Home</a-breadcrumb-item>
+        <a-breadcrumb-item>List</a-breadcrumb-item>
+        <a-breadcrumb-item>App</a-breadcrumb-item>
+      </a-breadcrumb>
+      <div class="tabBox" :style="{ background: '#fff', padding: '24px', minHeight: '500px' }">
+        <!-- 存储两个组件，左侧一个大的概念列表 -->
+         <!-- 右侧是关于模型相关属性的详细介绍，支持修改与重新提交 -->
+        <div class="mainBox">
+          <div class="leftConceptListBox">
+          <conceptListBox/>
+          </div>
+          <div class="rightConceptdetailBox">
+          
+          </div>
+        </div>
+      </div>
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
 </template>
-
 <script setup>
+import { ref } from 'vue';
+import conceptListBox from './conceptListBox.vue';
+const selectedKeys = ref(['1']);
 </script>
+<style scoped>
+.site-layout-content {
+  min-height: 280px;
+  padding: 24px;
+  background: #fff;
+}
+#components-layout-demo-top .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+.ant-row-rtl #components-layout-demo-top .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+[data-theme='dark'] .site-layout-content {
+  background: #141414;
+}
+
+.leftConceptListBox {
+  margin: 0 10px;
+  width: 250px;
+  border: #141414 solid 1px;
+}
+
+.rightConceptdetailBox {
+  margin: 0 10px;
+  width: 100%;
+  border: #141414 solid 1px;
+}
+
+.mainBox {
+  display: flex;
+}
+</style>
---
+++ conceptListDemo/src/components/conceptListBox.vue
@@ -0,0 +1,130 @@
+<template>
+    <a-anchor
+      :affix="false"
+      :items="items"
+      @change="onChange"
+    ></a-anchor>
+  </template>
+  <script setup>
+  import { ref } from 'vue';
+
+  let items = ref([
+        {
+          key: '1',
+          href: '#概念1',
+          title: 'Basic demo',
+        },
+        {
+          key: '2',
+          href: '#概念2',
+          title: 'Static demo',
+        },
+        {
+          key: '3',
+          href: '#概念3',
+          title: 'API',
+          children: [
+            {
+              key: '4',
+              href: '#小概念1',
+              title: 'Anchor Props',
+            },
+            {
+              key: '5',
+              href: '#小概念2',
+              title: 'Link Props',
+            },
+          ],
+        },
+        {
+          key: '6',
+          href: '#概念4',
+          title: 'API',
+          children: [
+            {
+              key: '7',
+              href: '#小概念3',
+              title: 'Anchor Props',
+            },
+            {
+              key: '8',
+              href: '#小概念4',
+              title: 'Link Props',
+            },
+          ],
+        },
+        {
+          key: '6',
+          href: '#概念4',
+          title: 'API',
+          children: [
+            {
+              key: '7',
+              href: '#小概念5',
+              title: 'Anchor Props',
+            },
+            {
+              key: '8',
+              href: '#小概念6',
+              title: 'Link Props',
+            },
+          ],
+        },
+        {
+          key: '6',
+          href: '#概念4',
+          title: 'API',
+          children: [
+            {
+              key: '7',
+              href: '#小概念7',
+              title: 'Anchor Props',
+            },
+            {
+              key: '8',
+              href: '#小概念8',
+              title: 'Link Props',
+            },
+          ],
+        },
+        {
+          key: '6',
+          href: '#概念4',
+          title: 'API',
+          children: [
+            {
+              key: '7',
+              href: '#小概念9',
+              title: 'Anchor Props',
+            },
+            {
+              key: '8',
+              href: '#小概念10',
+              title: 'Link Props',
+            },
+          ],
+        },
+        {
+          key: '6',
+          href: '#概念4',
+          title: 'API',
+          children: [
+            {
+              key: '7',
+              href: '#小概念11',
+              title: 'Anchor Props',
+            },
+            {
+              key: '8',
+              href: '#小概念12',
+              title: 'Link Props',
+            },
+          ],
+        },
+      ]);
+    //   herf是主要的链接，title是显示的文字，herf是唯一标识符，children是子节点，可以嵌套。
+    // 点击某个节点，会触发change事件，可以获取到当前节点的key和href。
+  const onChange = link => {
+    console.log('Anchor:OnChange', link);
+  };
+  </script>
